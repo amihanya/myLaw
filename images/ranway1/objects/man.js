8b25580135e441ec25980aff0c9f5452b2eeb346
Ami Hanya <amihanya@gmail.com>
2014-09-07 07:24:47 +0300

    more images

---
+++ images/ranway1/objects/man.js
@@ -0,0 +1,14 @@
+﻿(function(window) {
+Man_mc = function() {
+	this.initialize();
+}
+Man_mc._SpriteSheet = new createjs.SpriteSheet({images: ["images/ranway1/objects/man.png"], frames: [[0,0,151,201,0,0,0]]});
+var Man_mc_p = Man_mc.prototype = new createjs.BitmapAnimation();
+Man_mc_p.BitmapAnimation_initialize = Man_mc_p.initialize;
+Man_mc_p.initialize = function() {
+	this.BitmapAnimation_initialize(Man_mc._SpriteSheet);
+	this.paused = false;
+}
+window.Man_mc = Man_mc;
+}(window));
+
